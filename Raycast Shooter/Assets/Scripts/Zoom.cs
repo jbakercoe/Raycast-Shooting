750c7b79727a41db8ee330187f486b24c091e77b
Jbakercoe <jbakercoe@gmail.com>
2018-02-08 23:53:47 -0500

    Add Sniper Rife with Zoom Control
    Added a third gun (sniper rifle) with the ability to right click and look through scope.

---
+++ Raycast Shooter/Assets/Scripts/Zoom.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+    [SerializeField] private GameObject scopeScreen;
+    [SerializeField] private GameObject weaponCamera;
+    [SerializeField] private Camera mainCamera;
+
+    private bool isScoped = false;
+    private float scopedFOV = 10f;
+    private float normalFOV;
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Scope();
+        }
+		
+	}
+
+    void Scope()
+    {
+        isScoped = !isScoped;
+        if (!isScoped)
+        {
+            scopeScreen.gameObject.SetActive(false);
+            weaponCamera.gameObject.SetActive(true);
+            mainCamera.fieldOfView = normalFOV;
+        }
+        anim.SetBool("IsScoped", isScoped);
+    }
+
+    public void EngageScopeScreen()
+    {
+        scopeScreen.gameObject.SetActive(true);
+        weaponCamera.gameObject.SetActive(false);
+
+        normalFOV = mainCamera.fieldOfView;
+        mainCamera.fieldOfView = scopedFOV;
+    }
+
+}
